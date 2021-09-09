3a1fad330f2a1b2429f371e43afcf68901076195
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-09-09 22:35:23 +0900

    Stage 2/4: Networking

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Client started!");
         String address = "127.0.0.1";
